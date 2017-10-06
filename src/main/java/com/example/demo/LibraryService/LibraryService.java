14c52604d972c25967e1d61cc7a6e6f4113497f7
luopiao <piluo@thoughtworks.com>
2017-10-06 11:10:33 +0800

    [LuoPiao] add find movie by name logic

--- src/main/java/com/example/demo/LibraryService/LibraryService.java
+++ src/main/java/com/example/demo/LibraryService/LibraryService.java
@@ -1,18 +1,29 @@
 package com.example.demo.LibraryService;
 
 import com.example.demo.ResponseMessage.LoginMessage;
-import com.example.demo.ResponseMessage.MovieListMessage;
+import com.example.demo.util.CSVReader;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class LibraryService {
     private static final String template = "Dear %s! Welcome to our library!";
 
+    private CSVReader csvReader = new CSVReader();
+
     public LoginMessage login(String userName) {
         return new LoginMessage(userName, String.format(template, userName));
     }
 
-    public MovieListMessage getMovieList() {
-        return new MovieListMessage(null);
+    public List<String> getMovieList() {
+        List<Map<String,String>> allMoviesDetailList = csvReader.findAllMovies();
+        List<String> moviesList = new ArrayList<>();
+        for (Map<String, String> movie : allMoviesDetailList) {
+            moviesList.add(movie.get("movieName"));
+        }
+        return moviesList;
     }
 }
